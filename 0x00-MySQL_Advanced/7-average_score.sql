a872160cf3d79e980eb627561a82c6f546af840f
mahmoud malek <information.mahmoud@gmail.com>
2024-08-23 02:57:40 +0300

    fireUP :hammer:

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,22 +5,25 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-
+	
 	DECLARE total_score INT;
+	DECLARE total_projects INT;
+	DECLARE average_score DECIMAL(5,2);
+
 	SELECT SUM(score) INTO total_score
 	FROM corrections
 	WHERE user_id = user_id;
 
-	DECLARE total_projects INT;
+
 	SELECT COUNT(project_id) INTO total_projects
 	FROM corrections
 	WHERE user_id = user_id;
-
-	DECLARE average_score DECIMAL(5,2);
+	
 	SET average_score = total_score / total_projects;
 	
-	INSERT INTO users (average_score) VALUES (average_score)
-	WHERE id = user_id;
+	UPDATE users
+    SET average_score = average_score
+    WHERE id = user_id;
 
 END$$
 DELIMITER ;
