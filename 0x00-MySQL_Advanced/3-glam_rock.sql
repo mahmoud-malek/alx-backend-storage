52ca10281bc54d791c2d1466fbb1f3cc51de6687
mahmoud malek <information.mahmoud@gmail.com>
2024-08-23 01:06:24 +0300

    done :hammer:

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+--  a SQL script that lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+
+-- sql query
+SELECT band_name, 
+CASE
+	WHEN split IS NULL THEN 2022 - formed
+	ELSE split - formed
+END AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
