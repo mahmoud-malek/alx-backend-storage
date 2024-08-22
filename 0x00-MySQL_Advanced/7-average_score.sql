f1be07e68fcc17f5dbe09ff16e04e13dd0939f0b
mahmoud malek <information.mahmoud@gmail.com>
2024-08-23 02:49:43 +0300

    fireUP :hammer:

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,26 @@
+-- a SQL script that creates a stored procedure
+-- ComputeAverageScoreForUser that computes
+-- and store the average score for a student. Note: An average score can be a decimal
+
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+
+	DECLARE total_score INT;
+	SELECT SUM(score) INTO total_score
+	FROM scores
+	WHERE user_id = user_id;
+
+	DECLARE total_projects INT;
+	SELECT COUNT(project_id) INTO total_projects
+	FROM scores
+	WHERE user_id = user_id;
+
+	DECLARE average_score DECIMAL(5,2);
+	SET average_score = total_score / total_projects;
+	
+	INSERT INTO users (average_score) VALUES (average_score)
+	WHERE id = user_id;
+
+END$$
+DELIMITER ;
