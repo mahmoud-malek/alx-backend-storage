979b93e134ff00bdec34592c85e2cd38be121366
mahmoud malek <information.mahmoud@gmail.com>
2024-08-23 02:58:41 +0300

    again :hammer:

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,27 +1,30 @@
--- a SQL script that creates a stored procedure
+-- A SQL script that creates a stored procedure
 -- ComputeAverageScoreForUser that computes
--- and store the average score for a student. Note: An average score can be a decimal
+-- and stores the average score for a student. Note: An average score can be a decimal.
 
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-	
-	DECLARE total_score INT;
-	DECLARE total_projects INT;
-	DECLARE average_score DECIMAL(5,2);
+    
+    DECLARE total_score INT;
+    DECLARE total_projects INT;
+    DECLARE average_score DECIMAL(5,2);
 
-	SELECT SUM(score) INTO total_score
-	FROM corrections
-	WHERE user_id = user_id;
+    -- Calculate the total score for the user
+    SELECT SUM(score) INTO total_score
+    FROM corrections
+    WHERE user_id = user_id;
 
-
-	SELECT COUNT(project_id) INTO total_projects
-	FROM corrections
-	WHERE user_id = user_id;
-	
-	SET average_score = total_score / total_projects;
-	
-	UPDATE users
+    -- Calculate the total number of projects for the user
+    SELECT COUNT(project_id) INTO total_projects
+    FROM corrections
+    WHERE user_id = user_id;
+    
+    -- Compute the average score
+    SET average_score = total_score / total_projects;
+    
+    -- Update the user's average score
+    UPDATE users
     SET average_score = average_score
     WHERE id = user_id;
 
