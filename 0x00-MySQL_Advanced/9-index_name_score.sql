5f6eda403195694328d86d6e296abc23c3b01645
mahmoud malek <information.mahmoud@gmail.com>
2024-08-23 04:21:24 +0300

    FIRE_ALL :hammer:

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,6 +1,4 @@
--- a SQL script that creates an index idx_name_first_score
+-- creates an index idx_name_first_score
 -- on the table names and the first letter of name and the score.
-
-DROP INDEX IF EXISTS idx_name_first_score on names;
-CREATE INDEX idx_name_first_score ON names (name(1), score);
-
+CREATE INDEX idx_name_first_score
+ON names(name(1), score);
