82519d8a29ecafca77b5d9b4ba994922954bdfdf
mahmoud malek <information.mahmoud@gmail.com>
2024-08-23 03:06:55 +0300

    FireUp :hammer:

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,30 +4,30 @@
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN input_user_id INT)
 BEGIN
     
     DECLARE total_score INT;
     DECLARE total_projects INT;
-    DECLARE average_score DECIMAL(5,2);	
+    DECLARE average_score DECIMAL(5,2);
 
     -- Calculate the total score for the user
     SELECT SUM(score) INTO total_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = input_user_id;
 
     -- Calculate the total number of projects for the user
     SELECT COUNT(project_id) INTO total_projects
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = input_user_id;
     
     -- Compute the average score
-    SET average_score = total_score / total_projects;
+    SET average_score = CAST(total_score AS DECIMAL(5,2)) / total_projects;
     
     -- Update the user's average score
     UPDATE users
     SET average_score = average_score
-    WHERE id = user_id;
+    WHERE id = input_user_id;
 
 END$$
 DELIMITER ;
